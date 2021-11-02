2db79c6fc609a9e8a43bf8eb745b96505d5d3822
m1guelten <footballMiguelTen@gmail.com>
2021-11-02 18:49:53 +0200

    !!!

--- Home100.js
+++ Home100.js
@@ -1,10 +1,17 @@
 'use strict';
-console.clear;
-console.log('\x1b[10;10H');
-console.log(`
-                      ┌───────────────────────────────┐
-                      │ Login:                        │
-                      │ Password:                     │
-                      └───────────────────────────────┘
-`);
-let a=prompt('Enter login');
+
+process.on('uncaughtException', (err) => {
+    console.log('on uncaughtException: ' + err.message);
+  //  process.exit(1);
+  });
+  
+  const sum = (a, b) => {
+    if (typeof a === 'number' && typeof b === 'number') {
+      return a + b;
+    }
+    throw new Error('a and b should be numbers');
+  };
+  
+  console.log(sum(2, 3));
+  console.log(sum(7, 'A'));
+  console.log('Misha');
---
+++ proba2.js
@@ -0,0 +1,7 @@
+'use strict';
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '> ',
+  });
+let test = rl.prompt();
