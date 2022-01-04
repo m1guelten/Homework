6e45abe5f18846dfff0d9aeb842f3ced00cde27e
m1guelten <footballMiguelTen@gmail.com>
2022-01-04 21:07:20 +0200

    aa

--- Home100.js
+++ Home100.js
@@ -1,49 +1,23 @@
 'use strict';
 
-//#1
-const inc = x => ++x;
-const twice = x => x * 2;
-const cube = x => x ** 3;
+// Convert number to file size in bytes, kb, mb, and gb
 
+let size = (number) => {
+  if (number === 0) return '0';
+  if (number === undefined) return;
+  //else {
+  const exp = Math.floor(Math.log(number)/Math.log(1000));
+  if (exp===0) return number + ' byte';
+  if (exp===1) return Math.round(number / 1000) + ' kb';
+  if (exp===2) return Math.round(number / 1000000) + ' mb';
+  return Math.round(number / 1000000000) + ' gb';
+  //}
+}
 
-const pipe = (...fns) => {
-  for (const f of fns) {
-    if (typeof f !== 'function') {
-      throw new Error('Argument is not function');
-    }
-  }
-  return x => fns.reduce((v, f) => f(v), x);
-};
-
-
-
-const f = pipe(inc,twice,cube);
-const f2 = pipe(inc,inc);
-const f3 = pipe(inc,7,cube);
-console.log(`pipe(inc,twice,cube)= ${f(5)}`);
-console.log(`pipe(inc,inc)= ${f2(7)}`);
-//console.log(`pipe(inc,7,cube)= ${f3(9)}`);
-
-//#2
-const compose = (...fns) => {
-  const handlers = {};
-  const comp = x => {
-    if (fns.length === 0) {
-      return x;
-    }
-    let res = x;
-    try {
-      for (let i = (fns.length -1); i >= 0; i--) {
-        res = fns[i](res);
-      }
-      return res;
-    } 
-    catch (error) {
-    handlers.error=error;
-    }    
-  };
-  comp.on = (name) => {
-    if (name === 'error'){return handlers.error};
-  }
-  return comp;
-};
+console.log(size());
+console.log(size(0));
+console.log(size(123));
+console.log(size(123456));
+console.log(size(123956789));
+console.log(size(123456789000));
+console.log(size(1234567890120));
