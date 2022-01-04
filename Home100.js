2e86f0994633b3319f05cdc49db6c0bb775aeca5
m1guelten <footballMiguelTen@gmail.com>
2022-01-04 21:56:01 +0200

    home

--- Home100.js
+++ Home100.js
@@ -1,23 +1,23 @@
-'use strict';
+'use strict'
 
-// Convert number to file size in bytes, kb, mb, and gb
+// Get day number
 
-let size = (number) => {
-  if (number === 0) return '0';
-  if (number === undefined) return;
-  //else {
-  const exp = Math.floor(Math.log(number)/Math.log(1000));
-  if (exp===0) return number + ' byte';
-  if (exp===1) return Math.round(number / 1000) + ' kb';
-  if (exp===2) return Math.round(number / 1000000) + ' mb';
-  return Math.round(number / 1000000000) + ' gb';
-  //}
-}
+let week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
-console.log(size());
-console.log(size(0));
-console.log(size(123));
-console.log(size(123456));
-console.log(size(123956789));
-console.log(size(123456789000));
-console.log(size(1234567890120));
+ dayNumber= day =>{
+  for(let i=0;i<week.length;i++){
+    const dayLowerCase = day.toLowerCase();
+    if(dayLowerCase.startsWith(week[i].toLowerCase())){
+      return ++i;
+    };
+  };
+  return -1;
+};
+
+console.log(dayNumber ('sunday'));
+console.log(dayNumber ('Friday'));
+console.log(dayNumber ('fri'));
+console.log(dayNumber ('frid'));
+console.log(dayNumber ('fr'));
+console.log(dayNumber ('friday'));
+console.log(dayNumber ('friday'));
