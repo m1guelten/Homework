df3d93f7bda27d6433564005c2d747db5c288421
m1guelten <footballMiguelTen@gmail.com>
2022-05-27 17:58:46 +0300

    !1!

--- Home02.js
+++ Home02.js
@@ -1,90 +1,35 @@
-'use strict'
+'use strict';
 
+const MAX_NUMBER_MOVES = 32;
+const FULL_CIRCLE = 28;
+const MAX_MOVE_DOWN = 7;
+const MAX_MOVE_RIGHT = 14;
+const MAX_MOVE_UP = 21;
+const BIAS = 14;
+const ST_X = 0;
+const ST_Y = 0;
+const BOX = 1;
 
-//#1
-const random =(min=0,max) => {
-    if (max === undefined) {
-        max = min;
-        min = 0;
-    };
-    let x = Math.floor (Math.random()*(max-min)+min);
-    console.log (`Випадкове значення від ${min} до ${max} становить ${x}`);
-    return x;
-};
-//random (345);
-//random (567,23456)
+const koordB = new Array(MAX_NUMBER_MOVES);
+const koordW = new Array(MAX_NUMBER_MOVES);
 
-//#2
-const generateKey = (length, characters) => {
-    let y = characters.split ('');
-    let massiv =[];
-    for (let i=1;i<=length;i++) {
-        let z = Math.floor (Math.random()*(y.length-1));
-        massiv.push(y[z]);
-    };
-   return massiv.join('');
-};
-//console.log (generateKey (12, 'asdfghjk23456789'));
-//generateKey (12, 'asdfghjk23456789');
-//generateKey (5, 'mgkb3r46');
-
-//#3
-
-const ipZmin=(text='127.0.0.1')=>{
-    let b=text.split('.');
-//    console.log(b);
-    const zsuvLeft = (value, rozryad, number=1) =>{
-        for (let i=1; i<=number;i++){
-        let r = value << rozryad;
-        value = r;
-        };
-    return value;
-    };
-//    let massiv2=[];
-    let massiv3=[];
-    let j = 3;
-    for (let i=0;i<4;i++) {          
-        let f = parseInt(b[i],10);
-//      massiv2.push (f);
-        massiv3.push (zsuvLeft(f,8,j--));
-    };    
-//   console.log (massiv2);
-    console.log (massiv3);
-    let sum = massiv3.reduce(function (a,b){return a+b;});
-    return sum; 
-};
-//console.log (ipZmin('10.0.0.1'));
-//ipZmin('192.168.1.10');
-//ipZmin('165.225.133.150');
-//console.log(ipZmin());
-
-
-
-//#4
-
-const iface = {
-    city: 'Kyiv',
-    club: 'Dynamo',
-    year: 1927,
-    a1: x => [x],
-    a2: function (x, y) {
-        return [x, y];
-    },
-    a3(x, y, z) {
-        return [x, y, z];
-    }
+for (let i = 0; i <= FULL_CIRCLE; i++) {
+  if (i <= MAX_MOVE_DOWN) koordB[i] = { x: ST_X, y: ST_Y + BOX * i };
+  else if (i <= MAX_MOVE_RIGHT)
+    koordB[i] = { x: ST_X + BOX * (i - MAX_MOVE_DOWN), y: ST_Y + BOX * MAX_MOVE_DOWN };
+  else if (i <= MAX_MOVE_UP)
+    koordB[i] = { x: ST_X + BOX * MAX_MOVE_DOWN, y: ST_Y + BOX * (MAX_MOVE_UP - i) };
+  else koordB[i] = { x: ST_X + BOX * (FULL_CIRCLE - i), y: ST_Y };
+  i < BIAS ? (koordW[i + BIAS] = koordB[i]) : (koordW[i - BIAS] = koordB[i]);
 }
-//console.log (iface);
-const method = (iface) => { 
-    let massiv = [];
-    for (const name in iface) {
-        const mt = iface[name];
-        if (typeof mt==='function') {
-            massiv.push ([name, mt.length]);
-        }
-    }
-    return massiv;
+koordW[FULL_CIRCLE] = koordW[0];
+for (let i = 1; i < MAX_NUMBER_MOVES - FULL_CIRCLE; i++) {
+  koordB[i + FULL_CIRCLE] = { x: ST_X + BOX * i, y: ST_Y + BOX * i };
+  koordW[i + FULL_CIRCLE] = {
+    x: ST_X + BOX * (MAX_MOVE_DOWN - i),
+    y: ST_Y + BOX * (MAX_MOVE_DOWN - i),
+  };
 }
-//method (iface);
-console.log (method(iface)[0],method);
-
+for (let i = 0; i < 32; i++) {
+  console.log(`${i} Bx=${koordB[i].x} y=${koordB[i].y} Wx=${koordW[i].x} y=${koordW[i].y}`);
+}
